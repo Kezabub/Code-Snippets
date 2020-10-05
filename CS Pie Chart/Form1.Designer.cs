ef7e0365d58b8ecc7cf1c1297b96bb4c0c89b2c8
Kezabub <33518859+Kezabub@users.noreply.github.com>
2020-10-05 09:15:31 +0100

    Add files via upload

---
+++ CS Pie Chart/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Pie_Chart_Windows_Forms_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart1.Legends.Add(legend1);
+            this.Chart1.Location = new System.Drawing.Point(3, 6);
+            this.Chart1.Name = "Chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Chart1.Series.Add(series1);
+            this.Chart1.Size = new System.Drawing.Size(361, 321);
+            this.Chart1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 328);
+            this.Controls.Add(this.Chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart1;
+
+    }
+}
+
---
+++ CS Pie Chart/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace Pie_Chart_Windows_Forms_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Fetch the Statistical data from database.
+            string query = "SELECT ShipCity, COUNT(OrderId) [Total]";
+            query += " FROM Orders WHERE ShipCountry = 'Brazil'";
+            query += " GROUP BY ShipCity";
+            DataTable dt = GetData(query);
+
+            //Get the names of Cities.
+            string[] x = (from p in dt.AsEnumerable()
+                          orderby p.Field<string>("ShipCity") ascending
+                          select p.Field<string>("ShipCity")).ToArray();
+
+            //Get the Total of Orders for each City.
+            int[] y = (from p in dt.AsEnumerable()
+                       orderby p.Field<string>("ShipCity") ascending
+                       select p.Field<int>("Total")).ToArray();
+
+            Chart1.Series[0].ChartType = SeriesChartType.Pie;
+            Chart1.Series[0].Points.DataBindXY(x, y);
+            Chart1.Legends[0].Enabled = true;
+            Chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
+        }
+
+        private static DataTable GetData(string query)
+        {
+            string constr = @"Data Source=.\SQL2005;Initial Catalog=Northwind;User ID=sa;Password=pass@123";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter(query, con))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+    }
+}
